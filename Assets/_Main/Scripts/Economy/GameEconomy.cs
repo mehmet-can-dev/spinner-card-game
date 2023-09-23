89637c8f9c3d9f9abb24253d5e1e6753886ef9b6
Mehmet Can Çiçek <mehmet.can.cicek.dev@gmail.com>
2023-09-23 23:14:49 +0300

    [Added] basic-game-economy

---
+++ Assets/_Main/Scripts/Economy/GameEconomy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+public static class GameEconomy
+{
+    // ToDo integer max limit must be check or change currency type
+    // ToDo Temporary save PlayerPrefs 
+    private static int Coin
+    {
+        get => PlayerPrefs.GetInt(nameof(CurrencyType.COIN), 0);
+        set => PlayerPrefs.SetInt(nameof(CurrencyType.COIN), value);
+    }
+
+    // ToDo integer max limit must be check or change currency type
+    // ToDo Temporary save PlayerPrefs 
+    private static int Cash
+    {
+        get => PlayerPrefs.GetInt(nameof(CurrencyType.CASH), 0);
+        set => PlayerPrefs.SetInt(nameof(CurrencyType.CASH), value);
+    }
+
+    
+    //Temporary Functions
+    public static void AdjustCurrency(CurrencyType type, int amount)
+    {
+        switch (type)
+        {
+            case CurrencyType.COIN:
+                Coin += amount;
+                break;
+            case CurrencyType.CASH:
+                Cash += amount;
+                break;
+        }
+    }
+
+    //Temporary Functions
+    public static int GetCurrency(CurrencyType type)
+    {
+        switch (type)
+        {
+            case CurrencyType.COIN:
+                return Coin;
+            case CurrencyType.CASH:
+                return Cash;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(type), type, null);
+        }
+    }
+}
